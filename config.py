6130790958e2d2fe2c28f66109a496bdeca79220
v.mikhaylov <v.mikhaylov@adventum.ru>
2020-05-09 22:47:44 +0300

    debug=false

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ current_path = os.path.dirname(os.path.realpath(__file__))
 
 
 class Config:
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = os.environ.get('STEPIK_VOLUNTEERS_SECRET_KEY_API')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
